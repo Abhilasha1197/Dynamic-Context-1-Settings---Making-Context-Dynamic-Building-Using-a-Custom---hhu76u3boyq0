8fed2397db360c5e7e27007d13b53cf95d403d15
abhilasha <abhilasha15597@gmail.com>
2022-11-28 14:06:50 +0530

    new

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,19 +2,20 @@ import React from "react"
 import { useContext } from "react"
 import { UserContext } from "../context/userContext"
 
+
 export const Settings = () => {
 
     // to be implemented in context
 
-
+    
     const { changeGreeting } = useContext(UserContext)
-     const change =  e => UserContext(e.target.value);
+     
 
 
     return (
         <div style={{ border: '5px solid red', padding: '8px' }} id="settings">
             <h4>Settings</h4>
-            <input type={'text'} onChange ={"change"} value ={changeGreeting} />
+            <input type={'text'} value = {greeting} />
         </div>
     )
 }
